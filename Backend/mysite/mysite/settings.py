b2bf01a1ea463b9f98f4a8ffaa47bf9bde054f2b
Acrsahil <acharyasahil2003@gmail.com>
2026-03-03 18:57:31 +0545

    fix: COROS fixed for vercel

--- Backend/mysite/mysite/settings.py
+++ Backend/mysite/mysite/settings.py
@@ -321,8 +321,7 @@ CORS_ALLOWED_ORIGINS = [
 
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-     # ✅ ADD THIS:
-    "https://ama-bakery-8fffdneth-acrsahils-projects.vercel.app",
+    "https://ama-bakery-pos.vercel.app/"
 ]
 
 if RAILWAY_PUBLIC_DOMAIN:
